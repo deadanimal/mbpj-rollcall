21375a918c664790a348adbf4228ed54055dd7f9
noramirulnordin <noramirul.nordin@pipeline-network.com>
2022-06-21 18:11:17 +0800

    fix calendar

--- app/Models/Rollcall.php
+++ app/Models/Rollcall.php
@@ -20,15 +20,15 @@ class Rollcall extends Model
         return $this->belongsToMany(User::class, "penguatkuasa_id");
     }
 
-    // public function pegawai_sokong()
-    // {
-    //     return $this->belongsTo(User::class, 'pegawai_sokong_id', 'id');
-    // }
+    public function pegawai_sokong()
+    {
+        return $this->belongsTo(User::class, 'pegawai_sokong_id', 'id');
+    }
 
-    // public function pegawai_lulus()
-    // {
-    //     return $this->belongsTo(User::class, 'pegawai_lulus_id', 'id');
-    // }
+    public function pegawai_lulus()
+    {
+        return $this->belongsTo(User::class, 'pegawai_lulus_id', 'id');
+    }
 
     public function user_rollcall()
     {
